cee4878aef357c4af1a55c4857e1a1f93ec8c2c8
Alex Pagnoni <alex.pagnoni@innoteam.it>
2012-12-30 15:30:34 +0100

    Added vagrant file and chef coocbooks

---
+++ site-cookbooks/Innomatic/recipes/default.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: Innomatic
+# Recipe:: default
+#
+# Copyright 2012, Innoteam Srl
+#
+# All rights reserved - Do Not Redistribute
+#
+
+execute "disable-default-site" do
+  command "a2dissite default"
+end
+
+web_app "innomatic" do
+  application_name "innomatic-app"
+  docroot "/vagrant/source"
+end
