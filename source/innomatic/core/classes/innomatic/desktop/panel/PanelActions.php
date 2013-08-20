3af92fd4fbff401fa798339d2383a291163dc106
Alex Pagnoni <alex.pagnoni@innoteam.it>
2013-08-20 22:17:00 +0200

    New void Wui widget. New divframe Wui widget.

--- source/innomatic/core/classes/innomatic/desktop/panel/PanelActions.php
+++ source/innomatic/core/classes/innomatic/desktop/panel/PanelActions.php
@@ -51,4 +51,37 @@ abstract class PanelActions extends Observable
     {
         return $this->_controller;
     }
+    
+    public function ajaxInnomaticStickFrame($args) {    		
+    		$objResponse = new XajaxResponse();
+    		
+    		$name = $args[0];
+    		$top = $args[1];
+    		$left = $args[2];
+    	
+    		require_once('shared/wui/WuiDivframe.php');
+    		$empty = new WuiDivframe($name);
+    		$session_args = $empty->retrieveSession();
+
+    		if( isset($session_args['top']) ) {
+    			unset( $session_args['top'] );
+    			unset( $session_args['left'] );
+    			
+    			$sScript  = "var myImg = document.getElementById('pin_".$name."');";
+    			$sScript .= "myImg.src='".$empty->mThemeHandler->mIconsBase . $empty->mThemeHandler->mIconsSet['mini']['flag']['base'] . '/mini/' . $empty->mThemeHandler->mIconsSet['mini']['flag']['file']."';";
+    			$objResponse->addScript($sScript);
+    		} else {
+    			$session_args['top'] = $top;
+    			$session_args['left'] = $left;
+    			
+    			$sScript  = "var myImg = document.getElementById('pin_".$name."');";
+    			$sScript .= "myImg.src='".$empty->mThemeHandler->mIconsBase . $empty->mThemeHandler->mIconsSet['mini']['lock']['base'] . '/mini/' . $empty->mThemeHandler->mIconsSet['mini']['lock']['file']."';";
+    			$objResponse->addScript($sScript);
+    		}
+
+    		$empty->storeSession($session_args);
+    		
+    		return $objResponse->getXML();
+
+    }
 }
