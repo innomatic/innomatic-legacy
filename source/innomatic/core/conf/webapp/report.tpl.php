40d75427c82af063b8872dd4e153c6cc7bc23b16
Alex Pagnoni <alex.pagnoni@innoteam.it>
2013-09-30 22:56:48 +0200

    Changed status report style to new flat theme.

--- source/innomatic/core/conf/webapp/report.tpl.php
+++ source/innomatic/core/conf/webapp/report.tpl.php
@@ -5,19 +5,19 @@
 <head>
 <title><?=$title;?> - Error report</title>
 <style><!--
-* { font-family: "Lucida Grande", "Trebuchet MS", Verdana, Arial, Helvetica, sans-serif; }
-h1 { color: white; background-color: #525D76; font-size:22px; }
-h2 { color: white; background-color: #525D76; font-size:16px; }
-h3 { color: white; background-color: #525D76; font-size:14px; }
+* { font-family: "Helvetica Neue Ultra Light", Helvetica, Verdana, Arial, Geneva, sans-serif }
+h1 { color: white; font-weight: 500; background-color: #007aff; font-size:22px; padding: 8px; }
+h2 { color: white; font-weight: 500; background-color: #007aff; font-size:16px; padding: 4px; }
+h3 { color: white; font-weight: 500; background-color: #007aff; font-size:14px; padding: 4px; }
 body { color: #7f7e7e; background-color: white; }
-b { color: black; background-color: #eeeeee; }
-p { background: white; color: #7f7e7e; font-size:12px; }
+b { color: black; font-weight: 500; background-color: #eeeeee; }
+p { background: white; font-weight: 300; color: #7f7e7e; font-size:12px; }
 a { color: #7f7e7e; }
 hr { color: #525D76;}
 -->
 </style>
 </head><body>
-<h1>HTTP Status <?=$status_code.' - '.$message;?></h1>
+<h1>Innomatic - HTTP Status <?=$status_code.' - '.$message;?></h1>
 <hr size="1" noshade="noshade" />
 <p><b>type</b> <?=(is_null($e) ? 'Status report' : 'Exception report' );?></p>
 <p><b>message</b> <u><?php
