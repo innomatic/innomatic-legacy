9794475a2c6b0632c1b810adf1822e748de5590f
Alex Pagnoni <alex.pagnoni@innoteam.it>
2013-11-24 16:19:25 +0100

    INMATIC-58 Display the motd if set by the domain administrator

--- source/innomatic/core/classes/shared/dashboard/WelcomeDashboardWidget.php
+++ source/innomatic/core/classes/shared/dashboard/WelcomeDashboardWidget.php
@@ -3,8 +3,23 @@
 require_once 'innomatic/desktop/dashboard/DashboardWidget.php';
 
 class WelcomeDashboardWidget extends DashboardWidget {
-	public function getWidget() {
-		$xml = '<label><args><label>Welcome to Innomatic</label></args></label>';
+	public function getWidgetXml() {
+		// Get the message of the day
+		$message = InnomaticContainer::instance('innomaticcontainer')->getCurrentDomain()->getMotd();
+		
+		// Check if the motd is empty. If it is empty, get the generic welcome message
+		if (!strlen($message)) {
+			require_once('innomatic/locale/LocaleCatalog.php');
+			$catalog = new LocaleCatalog(
+					'innomatic::dashboard_welcome',
+					InnomaticContainer::instance('innomaticcontainer')->getCurrentUser()->getLanguage()
+			);
+			
+			$message = $catalog->getStr('welcome_title');
+		}	
+		
+		require_once 'shared/wui/WuiXml.php';
+		$xml = '<label><args><label>'.WuiXml::cdata($message).'</label></args></label>';
 		
 		return $xml;
 	}
