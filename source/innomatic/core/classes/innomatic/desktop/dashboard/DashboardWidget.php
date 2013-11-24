aa4810e293c39ea4414d9249e23e8e8ecc3ae0b9
Alex Pagnoni <alex.pagnoni@innoteam.it>
2013-11-24 16:06:15 +0100

    INMATIC-58 Fixed missing abstract keyword and wrong require.

--- source/innomatic/core/classes/innomatic/desktop/dashboard/DashboardWidget.php
+++ source/innomatic/core/classes/innomatic/desktop/dashboard/DashboardWidget.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once('innomatic/shared/wui/WuiXml.php');
+require_once('shared/wui/WuiXml.php');
 
 abstract class DashboardWidget {
-	public function getWidgetXml();
+	public abstract function getWidgetXml();
 }
