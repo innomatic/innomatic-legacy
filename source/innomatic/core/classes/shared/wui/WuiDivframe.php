76a736546041daba21ceaf6d83103a15d09db401
Alex Pagnoni <alex.pagnoni@innoteam.it>
2013-12-14 18:12:59 +0100

    Removed notices

--- source/innomatic/core/classes/shared/wui/WuiDivframe.php
+++ source/innomatic/core/classes/shared/wui/WuiDivframe.php
@@ -29,7 +29,7 @@ class WuiDivframe extends \Innomatic\Wui\Widgets\WuiContainerWidget
      * zindex
      *
      * isdraggable
-     * iscollapsible
+     * iscollapsable
      *
      * label
      * style
@@ -64,8 +64,25 @@ class WuiDivframe extends \Innomatic\Wui\Widgets\WuiContainerWidget
         if (! isset($this->mArgs['zindex']))
             $this->mArgs['zindex'] = 0;
 
-        if ( $this->mArgs['isdraggable'] == 'true' ) $this->mArgs['isdraggable'] = true; else $this->mArgs['isdraggable'] = false;
-        if ( $this->mArgs['iscollapsable'] == 'true' ) $this->mArgs['iscollapsable'] = true; else $this->mArgs['iscollapsable'] = false;
+        if (isset($this->mArgs['isdraggable'])) {
+	        if ( $this->mArgs['isdraggable'] == 'true' ) {
+	        	$this->mArgs['isdraggable'] = true;
+	        } else {
+	        	$this->mArgs['isdraggable'] = false;
+	        }
+        } else {
+        	$this->mArgs['isdraggable'] = false;
+        }
+        
+        if (isset($this->mArgs['iscollapsable'])) {
+        	if ( $this->mArgs['iscollapsable'] == 'true' ) {
+        		$this->mArgs['iscollapsable'] = true;
+        	} else {
+        		$this->mArgs['iscollapsable'] = false;
+        	}
+        } else {
+        	$this->mArgs['iscollapsable'] = false;
+        }
     }
 
     protected function generateSourceBegin()
@@ -75,27 +92,40 @@ class WuiDivframe extends \Innomatic\Wui\Widgets\WuiContainerWidget
 
         $style = 'style="';
 
-        if ((strlen($this->mArgs['display'])) and ($this->mArgs['display'] == "false")) {
+        if ((isset($this->mArgs['display'])) and ($this->mArgs['display'] == "false")) {
             $style .= 'display: none;';
         } else {
             $style .= 'display: block;';
         }
-        if ( strlen( $this->mArgs['bgcolor'] ) )
+        
+        if (strlen($this->mArgs['bgcolor'])) {
             $style .= ' background-color:'.$this->mArgs['bgcolor'].';';
+        }
 
-        if ( strlen( $this->mArgs['position'] ) )
+        if (isset($this->mArgs['position']) and strlen( $this->mArgs['position'])) {
             $style .= ' position:'.$this->mArgs['position'].';';
-        if ( strlen( $this->mArgs['top'] ) )
+        }
+        
+        if (isset($this->mArgs['top']) and strlen($this->mArgs['top'])) {
             $style .= ' top:'.$this->mArgs['top'].';';
-        if ( strlen( $this->mArgs['left'] ) )
+        }
+        
+        if (isset($this->mArgs['left']) and strlen( $this->mArgs['left'])) {
             $style .= ' left:'.$this->mArgs['left'].';';
-        if ( strlen( $this->mArgs['bottom'] ) )
+        }
+        
+        if (isset($this->mArgs['bottom']) and strlen( $this->mArgs['bottom'])) {
             $style .= ' bottom:'.$this->mArgs['bottom'].';';
-        if ( strlen( $this->mArgs['right'] ) )
+        }
+        
+        if (isset($this->mArgs['right']) and strlen($this->mArgs['right'])) {
             $style .= ' right:'.$this->mArgs['right'].';';
+        }
 
-        if ( strlen( $this->mArgs['border'] ) )
+        if (isset($this->mArgs['border']) and strlen( $this->mArgs['border'])) {
             $style .= ' border: '.$this->mArgs['border'].';';
+        }
+        
         $style .= 'z-index:'.$this->mArgs['zindex'].';';
         if ( $this->mArgs['isdraggable'] ) {
             $style .= 'cursor:move;';
@@ -109,12 +139,13 @@ class WuiDivframe extends \Innomatic\Wui\Widgets\WuiContainerWidget
         }
 
         // Generic style
-        if ( strlen( $this->mStyle ) )
+        if (isset($this->mArgs['style']) and strlen( $this->mArgs['style'])) {
             $style .= ' '.$this->mStyle;
+        }
 
-        $style .= ( $this->mArgs['iscollapsible'] ? '; overflow:hidden;" ' : '" ' );
+        $style .= ( $this->mArgs['iscollapsable'] ? '; overflow:hidden;" ' : '" ' );
 
-        if ($this->mArgs['iscollapsible']) {
+        if ($this->mArgs['iscollapsable']) {
             $session_args = $this->retrieveSession();
             if (\Innomatic\Core\InnomaticContainer::instance('\Innomatic\Core\InnomaticContainer')->isDomainStarted()) {
                 $language = \Innomatic\Core\InnomaticContainer::instance('\Innomatic\Core\InnomaticContainer')->getCurrentUser()->getLanguage();
@@ -128,17 +159,17 @@ class WuiDivframe extends \Innomatic\Wui\Widgets\WuiContainerWidget
         '<div '.
         ( $this->mArgs['isdraggable'] ? 'class="drag" ' : '' ).
         $style.' id="'.$this->mArgs['id'].'" '.( strlen( $this->mArgs['width'] ) ? 'width="'.$this->mArgs['width'].'" ' : '' ).">\n".
-        ( $this->mArgs['iscollapsible'] ? '&nbsp;<img title="'.$locale->getStr('fix_position').'" style="padding-left: 2px; padding-right: 2px; margin-bottom: 2px; float: left; cursor:hand; cursor:pointer;" id="pin_'.$this->mName.'" '.
+        ( $this->mArgs['iscollapsable'] ? '&nbsp;<img title="'.$locale->getStr('fix_position').'" style="padding-left: 2px; padding-right: 2px; margin-bottom: 2px; float: left; cursor:hand; cursor:pointer;" id="pin_'.$this->mName.'" '.
             ( isset($session_args['top']) ? 'src="'.$this->mThemeHandler->mIconsBase . $this->mThemeHandler->mIconsSet['mini']['lock']['base'] . '/mini/' . $this->mThemeHandler->mIconsSet['mini']['lock']['file'].'" '
                     : 'src="'.$this->mThemeHandler->mIconsBase . $this->mThemeHandler->mIconsSet['mini']['flag']['base'] . '/mini/' . $this->mThemeHandler->mIconsSet['mini']['flag']['file'].'" ').
             'onclick="javascript:xajax_InnomaticStickFrame(new Array(\''.$this->mName.'\',this.parentNode.offsetTop, this.parentNode.offsetLeft));">' : '' ).
-        ( strlen( $this->mArgs['label'] )
-            ? '<a '.( $this->mArgs['iscollapsible']
+        ((isset($this->mArgs['label']) and strlen($this->mArgs['label']))
+            ? '<a '.( $this->mArgs['iscollapsable']
                 ? 'onclick="javascript:var myTargetDiv = document.getElementById(\''.$this->mArgs['id'].'\'); '.
                 'if ( myTargetDiv.style.height == \'140px\' ) { myTargetDiv.style.height=\'44px\';} else { myTargetDiv.style.height=\'140px\';}" '
                 : '').
                 'style="padding-left:3px; color:#776666">'.
-                ( $this->mArgs['iscollapsible'] ? '<img title="'.$locale->getStr('minimize').'" style="margin-bottom: 2px; float: left; cursor: pointer; cursor:hand;" src="'.$this->mThemeHandler->mIconsBase . $this->mThemeHandler->mIconsSet['mini']['folder_red_open']['base'] . '/mini/' . $this->mThemeHandler->mIconsSet['mini']['folder_red_open']['file'].'">' : '' ).
+                ( $this->mArgs['iscollapsable'] ? '<img title="'.$locale->getStr('minimize').'" style="margin-bottom: 2px; float: left; cursor: pointer; cursor:hand;" src="'.$this->mThemeHandler->mIconsBase . $this->mThemeHandler->mIconsSet['mini']['folder_red_open']['base'] . '/mini/' . $this->mThemeHandler->mIconsSet['mini']['folder_red_open']['file'].'">' : '' ).
                 '<b>'.$this->mArgs['label'].'</b></a>'
             : '' );
 
