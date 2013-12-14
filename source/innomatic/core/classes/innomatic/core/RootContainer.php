345d517447d30e452c14a04ccb3b97dffdc26480
Alex Pagnoni <alex.pagnoni@innoteam.it>
2013-12-14 17:57:47 +0100

    Fixed handling of uppercase class path.

--- source/innomatic/core/classes/innomatic/core/RootContainer.php
+++ source/innomatic/core/classes/innomatic/core/RootContainer.php
@@ -210,14 +210,14 @@ class RootContainer extends \Innomatic\Util\Singleton
 		if ($lastNsPos = strrpos($className, '\\')) {
 			$namespace = substr($className, 0, $lastNsPos);
 			$className = substr($className, $lastNsPos + 1);
-			$fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+			$fileName  = strtolower(str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR);
 		}
 		
 		if (isset($GLOBALS['system_classes'][strtolower($className)]))
 		{
 			$fileName = $GLOBALS['system_classes'][strtolower($className)]['path'];
 		}
-		else 
+		else
 		{
 			$fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
 		}
