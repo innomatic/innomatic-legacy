cd76edb28bec5314ae5922f621e0dc464d6fcb92
Alex Pagnoni <alex.pagnoni@innoteam.it>
2014-02-23 13:41:51 +0100

    Added licence header

--- source/innomatic/core/classes/innomatic/domain/user/Role.php
+++ source/innomatic/core/classes/innomatic/domain/user/Role.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * Innomatic
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.
+ *
+ * @copyright  1999-2014 Innoteam Srl
+ * @license    http://www.innomatic.org/license/   BSD License
+ * @link       http://www.innomatic.org
+ * @since      Class available since Release 6.4.0
+ */
 namespace Innomatic\Domain\User;
 
 use \Innomatic\Core;
