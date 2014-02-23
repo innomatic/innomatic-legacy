cd3a309a14382c8731e2e5da65e7aeefe9e958f5
Alex Pagnoni <alex.pagnoni@innoteam.it>
2014-02-23 13:41:39 +0100

    Fixed version

--- source/innomatic/core/classes/innomatic/desktop/auth/DesktopPanelAuthorizator.php
+++ source/innomatic/core/classes/innomatic/desktop/auth/DesktopPanelAuthorizator.php
@@ -10,7 +10,7 @@
  * @copyright  1999-2014 Innoteam Srl
  * @license    http://www.innomatic.org/license/   BSD License
  * @link       http://www.innomatic.org
- * @since      Class available since Release 5.0
+ * @since      Class available since Release 6.4.0
 */
 namespace Innomatic\Desktop\Auth;
 
