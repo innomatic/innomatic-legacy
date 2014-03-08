dd12c0dd93a91d79239b64b3577a57fade0c3c7b
Alex Pagnoni <alex.pagnoni@innoteam.it>
2014-03-08 15:54:15 +0100

    Improved class loader compatibility layer handling of classes with the same name in different namespaces.

--- source/innomatic/core/classes/innomatic/core/RootContainer.php
+++ source/innomatic/core/classes/innomatic/core/RootContainer.php
@@ -99,64 +99,65 @@ class RootContainer extends \Innomatic\Util\Singleton
     {
         return $this->clean;
     }
-    
+
     /**
      * SPL autoload method.
-     * 
+     *
      * @since 6.1
      * @param string $class_name
      */
     public static function autoload($class_name)
-    {
-        if (strpos($class_name, '\\') !== false) {
-            $orig = $class_name;
-            $class_pop = explode('\\', $class_name);
-            $class_name = array_pop($class_pop);
-            $file = self::getClassFile($orig);
-        } else {
-            $skip = false;
-            $file = self::getClassFile($class_name);
-        }
-        // use some function to find the file that declares the class requested
-        
-        // remember the defined classes, include the $file and detect newly declared classes
-        $pre = array_merge(get_declared_classes(), get_declared_interfaces());
-        require_once($file);
-        $post = array_unique(array_diff(array_merge(get_declared_classes(), get_declared_interfaces()), $pre));
-        
-        // loop through the new class definitions and create weak aliases if they are given with qualified names
-        foreach ($post as $cd) {
-            $d = explode('\\', $cd);
-            if (count($d) > 1) {
-                // Aliasing full qualified classnames to their simple ones. Note: weak alias!
-                self::createClassAlias($cd, array_pop($d));
-            }
-        }
-        
-        // get the class definition. note: we assume that there's only one class/interface in each file!
-        $def = array_pop($post);
-        if (!isset($orig) && !$def) {
-            // plain class requested AND file was already included, so search up the declared classes and alias
-            foreach (array_reverse($pre) as $c) {
-                if (!(strtolower(substr($c, strlen($c)-strlen($class_name))) == strtolower($class_name))) {
-                    continue;
-                }
-                // Aliasing previously included class
-                self::createClassAlias($c, $class_name, true);
-                break;
-            }
-        } elseif (isset($orig) && !$def) {
-            self::createClassAlias($class_name, $orig, true);
-        } else {
-            $class_name = isset($orig)?$orig:$class_name;
-            if (strtolower($def) != strtolower($class_name) && strtolower(substr($def,strlen($def)-strlen($class_name))) == strtolower($class_name))
-            // no qualified classname requested but class was defined with namespace
-            {
-                // Aliasing class
-                self::createClassAlias($def,$class_name,true);
-            }
-        }
-    }
+	{
+	    if (strpos($class_name, '\\') !== false) {
+	        $orig = $class_name;
+	        $class_pop = explode('\\',$class_name);
+	        $class_name = array_pop($class_pop);
+	        $file = self::getClassFile($orig);
+	    } else {
+	        $skip = false;
+	    	$file = self::getClassFile($class_name);
+	    }
+	    // use some function to find the file that declares the class requested
+
+	    // remember the defined classes, include the $file and detect newly declared classes
+	    $pre = array_merge(get_declared_classes(), get_declared_interfaces());
+	    require_once($file);
+	    $post = array_unique(array_diff(array_merge(get_declared_classes(), get_declared_interfaces()), $pre));
+
+	    // loop through the new class definitions and create weak aliases if they are given with qualified names
+	    foreach ($post as $cd) {
+	        $d = explode('\\',$cd);
+	        if (count($d) > 1) {
+	            // Aliasing full qualified classnames to their simple ones. Note: weak alias!
+	            self::createClassAlias($cd,array_pop($d));
+	        }
+	    }
+
+	    // get the class definition. note: we assume that there's only one class/interface in each file!
+	    $def = array_pop($post);
+	    if (!isset($orig) && !$def)
+	    // plain class requested AND file was already included, so search up the declared classes and alias
+	    {
+	        foreach( array_reverse($pre) as $c ) {
+	            if (!(strtolower(substr($c,strlen($c)-strlen($class_name))) == strtolower($class_name))) {
+	                continue;
+	            }
+	            // Aliasing previously included class
+	            self::createClassAlias($c,$class_name,true);
+	            break;
+	        }
+	    } elseif (isset($orig) && !$def) {
+	    	self::createClassAlias($class_name,$orig,true);
+	    } else {
+	        $class_name = isset($orig)?$orig:$class_name;
+	        if (strtolower($def) != strtolower($class_name) && strtolower(substr($def,strlen($def)-strlen($class_name))) == strtolower($class_name))
+	        // no qualified classname requested but class was defined with namespace
+	        {
+	            // Aliasing class
+	            self::createClassAlias($def,$class_name,true);
+	        }
+	    }
+	}
 
     public static function getApplicationClasses($file)
     {
@@ -164,7 +165,8 @@ class RootContainer extends \Innomatic\Util\Singleton
         $file = new \SimpleXMLElement($xml);
 
         $application_classes = array();
-        
+        $application_fqcns = array();
+
         foreach($file->components->class as $class) {
             $path = "{$class['name']}";
             $elements = explode('/', $path);
@@ -175,11 +177,12 @@ class RootContainer extends \Innomatic\Util\Singleton
                     $match = ucfirst($match);
                 }
             );
-            
+
             $fqcn = (count($elements) ? '\\'.implode('\\', $elements) : '').'\\'.$class;
             $application_classes[strtolower($class)] = array('path' => $path, 'fqcn' => $fqcn);
+            $application_fqcns[$fqcn] = $fqcn;
         }
-        
+
         foreach($file->components->wuiwidget as $class) {
             $path = "shared/wui/{$class['file']}";
             $elements = explode('/', $path);
@@ -190,83 +193,90 @@ class RootContainer extends \Innomatic\Util\Singleton
                     $match = ucfirst($match);
                 }
             );
-        
+
             $fqcn = (count($elements) ? '\\'.implode('\\', $elements) : '').'\\'.$class;
             $application_classes[strtolower($class)] = array('path' => $path, 'fqcn' => $fqcn);
+            $application_fqcns[$fqcn] = $fqcn;
         }
 
-        return $application_classes;
+        return array('classes' => $application_classes, 'fqcns' => $application_fqcns);
     }
 
-    public static function getClassFile($className)
-    {
-        $registry = \Innomatic\Util\Registry::instance();
-        // Backwards compatibility system
-        if (!$registry->isGlobalObject('system_classes')) {
-            $system_classes = array();
+	public static function getClassFile($className)
+	{
+		$registry = \Innomatic\Util\Registry::instance();
+		// Backwards compatibility system
+		if (!$registry->isGlobalObject('system_classes')) {
+			$system_classes = $system_fqcns = array();
             if (file_exists('innomatic/setup/application.xml')) {
-                $system_classes = self::getApplicationClasses('innomatic/setup/application.xml');
+                $application_classes = self::getApplicationClasses('innomatic/setup/application.xml')['classes'];
+                $system_classes = $application_classes['classes'];
+                $system_fqcns = $application_classes['fqcns'];
             }
-            if ($handle = opendir('innomatic/core/applications/')) {
-                while (false !== ($entry = readdir($handle))) {
-                    if (
-                        $entry != "."
-                        && $entry != ".."
-                        && is_dir('innomatic/core/applications/'.$entry)
-                        && file_exists('innomatic/core/applications/'.$entry.'/application.xml')
-                    ) {
+			if ($handle = opendir('innomatic/core/applications/')) {
+				while (false !== ($entry = readdir($handle))) {
+					if (
+						$entry != "."
+						&& $entry != ".."
+						&& is_dir('innomatic/core/applications/'.$entry)
+						&& file_exists('innomatic/core/applications/'.$entry.'/application.xml')
+					) {
                         $application_classes = self::getApplicationClasses('innomatic/core/applications/'.$entry.'/application.xml');
-                        $system_classes = array_merge($application_classes, $system_classes);
-                    }
-                }
-                
-                closedir($handle);
-            }
+                        $system_classes = array_merge($application_classes['classes'], $system_classes);
+                        $system_fqcns = array_merge($application_classes['fqcns'], $system_fqcns);
+					}
+				}
+
+				closedir($handle);
+			}
             $registry->setGlobalObject('system_classes', $system_classes);
-        }
-        
-        $className = ltrim($className, '\\');
-        $fileName  = '';
-        $namespace = '';
-        if ($lastNsPos = strrpos($className, '\\')) {
-            $namespace = substr($className, 0, $lastNsPos);
-            $className = substr($className, $lastNsPos + 1);
-            $fileName  = strtolower(str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR);
-        }
+            $registry->setGlobalObject('system_fqcns', $system_fqcns);
+		}
+
+		$className = $fqcn = ltrim($className, '\\');
+		$fileName  = '';
+		$namespace = '';
+		if ($lastNsPos = strrpos($className, '\\')) {
+			$namespace = substr($className, 0, $lastNsPos);
+			$className = substr($className, $lastNsPos + 1);
+			$fileName  = strtolower(str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR);
+		}
 
         $reg = $registry->getGlobalObject('system_classes');
-        if (isset($reg[strtolower($className)])){
-            $fileName = $reg[strtolower($className)]['path'];
-        } else {
-            $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
-        }
-        return $fileName;
-    }
-    
-    public static function createClassAlias($original, $alias, $strong=false)
-    {
-        // if strong create a real alias known to PHP
-        if ($strong) {
-            if (!interface_exists($original)) {
-                class_alias($original,$alias);
-            }
-        }
-    
-        // In any case store the alias in a global variable
-        $alias = strtolower($alias);
-        
-        if (isset($GLOBALS['system_class_alias'][$alias])) {
-            if ($GLOBALS['system_class_alias'][$alias] == $original) {
-                return;
-            }
-    
-            if (!is_array($GLOBALS['system_class_alias'][$alias])) {
-                $GLOBALS['system_class_alias'][$alias] = array($GLOBALS['system_class_alias'][$alias]);
-            }
-            
-            $GLOBALS['system_class_alias'][$alias][] = $original;
-        } else {
-            $GLOBALS['system_class_alias'][$alias] = $original;
-        }
-    }
+        $fqcns = $registry->getGlobalObject('system_fqcns');
+
+		if (!isset($fqcns['\\'.$fqcn]) && isset($reg[strtolower($className)])) {
+			$fileName = $reg[strtolower($className)]['path'];
+		} else {
+			$fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+		}
+		return $fileName;
+	}
+
+	public static function createClassAlias($original, $alias, $strong=false)
+	{
+		// if strong create a real alias known to PHP
+		if ($strong) {
+			if (!interface_exists($original)) {
+				class_alias($original,$alias);
+			}
+		}
+
+		// In any case store the alias in a global variable
+		$alias = strtolower($alias);
+
+		if (isset($GLOBALS['system_class_alias'][$alias])) {
+			if ($GLOBALS['system_class_alias'][$alias] == $original) {
+				return;
+			}
+
+			if (!is_array($GLOBALS['system_class_alias'][$alias])) {
+				$GLOBALS['system_class_alias'][$alias] = array($GLOBALS['system_class_alias'][$alias]);
+			}
+
+			$GLOBALS['system_class_alias'][$alias][] = $original;
+		} else {
+			$GLOBALS['system_class_alias'][$alias] = $original;
+		}
+	}
 }
