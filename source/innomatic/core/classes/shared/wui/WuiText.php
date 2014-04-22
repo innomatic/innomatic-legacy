13caa54822b6b034a924dac7fa230283befbfa8d
Edoardo Spadoni <edoardo.spadoni@innoteam.it>
2014-04-22 11:05:13 +0200

    Added maxlength attribute to WuiText.php to limit the number of character

--- source/innomatic/core/classes/shared/wui/WuiText.php
+++ source/innomatic/core/classes/shared/wui/WuiText.php
@@ -66,5 +66,6 @@ requiredFields[requiredFields.length] = new Array( "' . $event_data->getDataStri
         $this->mLayout = ($this->mComments ? '<!-- begin ' . $this->mName . ' textarea -->' : '') . '<textarea'.(isset($this->mArgs['id']) ? ' id="'.$this->mArgs['id'].'"' : '').' '.(isset($this->mArgs['maxlength']) ? ' maxlength="'.$this->mArgs['maxlength'].'"' : '') . ((isset($this->mArgs['hint']) and strlen($this->mArgs['hint'])) ? 'onMouseOver="wuiHint(\'' . str_replace("'", "\'", $this->mArgs['hint']) . '\');" onMouseOut="wuiUnHint();" ' : '') . 'name="' . $event_data->getDataString() . '"' . (strlen($this->mArgs['rows']) ? ' rows="' . $this->mArgs['rows'] . '"' : '') . (strlen($this->mArgs['bgcolor']) ? ' STYLE="background-color: ' . $this->mArgs['bgcolor'] . ';"' : '') . (strlen($this->mArgs['cols']) ? ' cols="' . $this->mArgs['cols'] . '"' : '') . ' tabindex="' . $this->mArgs['tabindex'] . '"' . ((isset($this->mArgs['readonly']) and strlen($this->mArgs['readonly'])) ? ' readonly' : '') . '>' . ((isset($this->mArgs['value']) and strlen($this->mArgs['value'])) ? \Innomatic\Wui\Wui::utf8_entities($this->mArgs['value']) : '') . '</textarea>' . $check_script . ($this->mComments ? '<!-- end ' . $this->mName . " textarea -->\n" : '');
         $result = true;
         return $result;
+        
     }
 }
