0cf64cf543bd3f6c06fb6ee3f8a0863de20f82e8
Alex Pagnoni <alex.pagnoni@innoteam.it>
2014-08-22 14:51:45 +0200

    Added file code header

--- source/innomatic/core/scripts/phpunit_bootstrap.php
+++ source/innomatic/core/scripts/phpunit_bootstrap.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * Innomatic
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.
+ *
+ * @copyright  1999-2014 Innoteam Srl
+ * @license    http://www.innomatic.org/license/   BSD License
+ * @link       http://www.innomatic.org
+ * @since      6.4.0
+ */
 
 require_once('scripts_container.php');
 
