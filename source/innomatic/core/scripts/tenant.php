117a04339ca14336fc664324bd483fa70dcbff40
Alex Pagnoni <alex.pagnoni@innoteam.it>
2014-08-24 17:40:06 +0200

    Renamed domain.php script to tenant.php

--- source/innomatic/core/scripts/tenant.php
+++ source/innomatic/core/scripts/tenant.php
@@ -20,22 +20,22 @@ if (! isset($argv[1]))
 try {
     switch ($argv[1]) {
         case '-h':
-            print('Usage: php innomatic/core/scripts/domain.php command argument' . "\n");
+            print('Usage: php innomatic/core/scripts/tenant.php command argument' . "\n");
             print("\n");
             print('Supported commands:' . "\n");
-            print('    create domainname description pwd  Creates a new domain' . "\n");
-            print('    remove domainname                  Removes a domain' . "\n");
-            print('    enable domainname                  Enables a disabled domain' . "\n");
-            print('    disable domainname                 Disables a domain' . "\n");
-            print('    applist domainname                 Retrieves a list of enabled applications' . "\n");
-            print('    appenable domainname application   Disables a domain' . "\n");
-            print('    appdisable domainname application  Disables a domain' . "\n");
+            print('    create tenantname description pwd  Creates a new tenant' . "\n");
+            print('    remove tenantname                  Removes a tenant' . "\n");
+            print('    enable tenantname                  Enables a disabled tenant' . "\n");
+            print('    disable tenantname                 Disables a tenant' . "\n");
+            print('    applist tenantname                 Retrieves a list of enabled applications' . "\n");
+            print('    appenable tenantname application   Disables a tenant' . "\n");
+            print('    appdisable tenantname application  Disables a tenant' . "\n");
             $script->cleanExit();
             break;
         
         case 'enable':
-            $domain = new \Innomatic\Domain\Domain(InnomaticContainer::instance('\Innomatic\Core\InnomaticContainer')->getDataAccess(), $argv[2], null);
-            if ($domain->enable()) {
+            $tenant = new \Innomatic\Domain\Domain(InnomaticContainer::instance('\Innomatic\Core\InnomaticContainer')->getDataAccess(), $argv[2], null);
+            if ($tenant->enable()) {
                 print("Domain $argv[2] enabled\n");
                 $script->cleanExit();
             } else {
@@ -45,8 +45,8 @@ try {
             break;
         
         case 'disable':
-            $domain = new \Innomatic\Domain\Domain(InnomaticContainer::instance('\Innomatic\Core\InnomaticContainer')->getDataAccess(), $argv[2], null);
-            if ($domain->disable()) {
+            $tenant = new \Innomatic\Domain\Domain(InnomaticContainer::instance('\Innomatic\Core\InnomaticContainer')->getDataAccess(), $argv[2], null);
+            if ($tenant->disable()) {
                 print("Domain $argv[2] disabled\n");
                 $script->cleanExit();
             } else {
@@ -56,12 +56,12 @@ try {
             break;
         
         case 'create':
-            $domain = new \Innomatic\Domain\Domain(InnomaticContainer::instance('\Innomatic\Core\InnomaticContainer')->getDataAccess(), 0, null);
+            $tenant = new \Innomatic\Domain\Domain(InnomaticContainer::instance('\Innomatic\Core\InnomaticContainer')->getDataAccess(), 0, null);
             $data['domainid'] = $argv[2];
             $data['domainname'] = $argv[3];
             $data['domainpassword'] = $argv[4];
             
-            if ($domain->create($data)) {
+            if ($tenant->create($data)) {
                 print("Domain $argv[2] created\n");
                 $script->cleanExit();
             } else {
@@ -71,8 +71,8 @@ try {
             break;
         
         case 'remove':
-            $domain = new \Innomatic\Domain\Domain(InnomaticContainer::instance('\Innomatic\Core\InnomaticContainer')->getDataAccess(), $argv[2], null);
-            if ($domain->remove()) {
+            $tenant = new \Innomatic\Domain\Domain(InnomaticContainer::instance('\Innomatic\Core\InnomaticContainer')->getDataAccess(), $argv[2], null);
+            if ($tenant->remove()) {
                 print("Domain $argv[2] removed\n");
                 $script->cleanExit();
             } else {
@@ -82,8 +82,8 @@ try {
             break;
         
         case 'applist':
-            $domain = new \Innomatic\Domain\Domain(InnomaticContainer::instance('\Innomatic\Core\InnomaticContainer')->getDataAccess(), $argv[2], null);
-            $list = $domain->getEnabledApplications();
+            $tenant = new \Innomatic\Domain\Domain(InnomaticContainer::instance('\Innomatic\Core\InnomaticContainer')->getDataAccess(), $argv[2], null);
+            $list = $tenant->getEnabledApplications();
             foreach ($list as $app) {
                 print($app . "\n");
             }
@@ -91,32 +91,32 @@ try {
             break;
         
         case 'appenable':
-            $domain = new \Innomatic\Domain\Domain(InnomaticContainer::instance('\Innomatic\Core\InnomaticContainer')->getDataAccess(), $argv[2], null);
+            $tenant = new \Innomatic\Domain\Domain(InnomaticContainer::instance('\Innomatic\Core\InnomaticContainer')->getDataAccess(), $argv[2], null);
             $appid = \Innomatic\Application\Application::getAppIdFromName($argv[3]);
-            if ($domain->enableApplication($appid)) {
-                print("Application $argv[3] enabled to domain $argv[2]\n");
+            if ($tenant->enableApplication($appid)) {
+                print("Application $argv[3] enabled to tenant $argv[2]\n");
                 $script->cleanExit();
             } else {
-                print("Application $argv[3] not enabled to domain $argv[2]\n");
+                print("Application $argv[3] not enabled to tenant $argv[2]\n");
                 $script->cleanExit(1);
             }
             break;
         
         case 'appdisable':
-            $domain = new \Innomatic\Domain\Domain(InnomaticContainer::instance('\Innomatic\Core\InnomaticContainer')->getDataAccess(), $argv[2], null);
+            $tenant = new \Innomatic\Domain\Domain(InnomaticContainer::instance('\Innomatic\Core\InnomaticContainer')->getDataAccess(), $argv[2], null);
             $appid = \Innomatic\Application\Application::getAppIdFromName($argv[3]);
-            if ($domain->disableApplication($appid)) {
-                print("Application $argv[3] disabled from domain $argv[2]\n");
+            if ($tenant->disableApplication($appid)) {
+                print("Application $argv[3] disabled from tenant $argv[2]\n");
                 $script->cleanExit();
             } else {
-                print("Application $argv[3] not disabled from domain $argv[2]\n");
+                print("Application $argv[3] not disabled from tenant $argv[2]\n");
                 $script->cleanExit(1);
             }
             break;
         
         default:
-            print('Usage: php innomatic/core/scripts/domain.php command' . "\n");
-            print('Type domain.php -h for a list of supported commands' . "\n");
+            print('Usage: php innomatic/core/scripts/tenant.php command' . "\n");
+            print('Type tenant.php -h for a list of supported commands' . "\n");
     }
 } catch (\Exception $e) {
     echo $e;
