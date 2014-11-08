47abcbab2cf05ea7144b88f549c366633b9e04ad
Alex Pagnoni <alex.pagnoni@innoteam.it>
2014-11-08 13:10:20 +0100

    Improved code readability and comments

--- source/innomatic/core/classes/shared/components/MaintenancetaskComponent.php
+++ source/innomatic/core/classes/shared/components/MaintenancetaskComponent.php
@@ -16,39 +16,129 @@ namespace Shared\Components;
 
 /**
  * Maintenance task component handler.
+ *
+ * A maintenance task component is used to define the actions to be done in
+ * planned maintenance with a cronjob.
+ *
+ * Component parameters:
+ *
+ * - name: name of the class that extends the Maintenance task component
+ * - file: name of the class file, without path
+ * - catalog: name of catalog used to translate the text
+ * - enabled: defines if the scheduled task is enabled by default or not
  */
 class MaintenancetaskComponent extends \Innomatic\Application\ApplicationComponent
 {
+    /* public __construct($rootda, $domainda, $appname, $name, $basedir) {{{ */
+    /**
+     * Class constructor.
+     *
+     * @param \Innomatic\Dataaccess\DataAccess $rootda Innomatic root data access.
+     * @param \Innomatic\Dataaccess\DataAccess $domainda Tenant data access.
+     * @param string $appname Application name identifier.
+     * @param string $name Component name.
+     * @param string $basedir Temporary directory containing the application extracted archive.
+     */
     public function __construct($rootda, $domainda, $appname, $name, $basedir)
     {
         parent::__construct($rootda, $domainda, $appname, $name, $basedir);
     }
+    /* }}} */
+
+    /* public getType() {{{ */
+    /**
+     * Gets the component type identifier string.
+     *
+     * @static
+     * @access public
+     * @return string
+     */
     public static function getType()
     {
         return 'maintenancetask';
     }
+    /* }}} */
+
+    /* public getPriority() {{{ */
+    /**
+     * Gets the priority applied when executing operations with
+     * the component.
+     *
+     * @static
+     * @access public
+     * @return integer
+     */
     public static function getPriority()
     {
         return 0;
     }
+    /* }}} */
+
+    /* public getIsDomain() {{{ */
+    /**
+     * Checks if the component can be enabled to/disabled from tenants.
+     *
+     * @static
+     * @access public
+     * @return boolean
+     */
     public static function getIsDomain()
     {
         return false;
     }
+    /* }}} */
+
+    /* public getIsOverridable() {{{ */
+    /**
+     * Checks if the component supports custom overrides.
+     *
+     * @static
+     * @access public
+     * @return boolean
+     */
     public static function getIsOverridable()
     {
         return false;
     }
+    /* }}} */
+
+    /* public doInstallAction($args) {{{ */
+    /**
+     * Executes component install action.
+     *
+     * It should be called by install() method only. It should be redefined
+     * by the extending class.
+     *
+     * @param array $params Parameters in the component definition
+     * @access public
+     * @return bool True if not extended.
+     */
     public function doInstallAction($args)
     {
         $result = false;
+
         $args['file'] = $this->basedir . '/core/classes/shared/maintenance/' . basename($args['file']);
+
         if (strlen($args['name']) and strlen($args['file']) and file_exists($args['file'])) {
-            if (! isset($args['enabled']))
+            if (!isset($args['enabled'])) {
                 $args['enabled'] = 'false';
-            if (! isset($args['catalog']))
+            }
+
+            if (!isset($args['catalog'])) {
                 $args['catalog'] = '';
-            $result = $this->container->getDataAccess()->execute('INSERT INTO maintenance_tasks VALUES (' . $this->container->getDataAccess()->formatText($args['name']) . ',' . $this->container->getDataAccess()->formatText(basename($args['file'])) . ',' . $this->container->getDataAccess()->formatText($args['catalog']) . ',' . $this->container->getDataAccess()->formatText($args['enabled'] == 'true' ? $this->container->getDataAccess()->fmttrue : $this->container->getDataAccess()->fmtfalse) . ')');
+            }
+
+            $result = $this->container->getDataAccess()->execute(
+                'INSERT INTO maintenance_tasks '.
+                'VALUES (' . $this->container->getDataAccess()->formatText($args['name']) .
+                ',' . $this->container->getDataAccess()->formatText(basename($args['file'])) .
+                ',' . $this->container->getDataAccess()->formatText($args['catalog']) .
+                ',' . $this->container->getDataAccess()->formatText(
+                    $args['enabled'] == 'true' ? $this->container->getDataAccess()->fmttrue : $this->container->getDataAccess()->fmtfalse
+                ) .
+                ')'
+            );
+
             if ($result) {
                 @copy($args['file'], $this->container->getHome() . 'core/classes/shared/maintenance/' . basename($args['file']));
                 @chmod($this->container->getHome() . 'core/classes/shared/maintenance/' . basename($args['file']), 0644);
@@ -56,30 +146,69 @@ class MaintenancetaskComponent extends \Innomatic\Application\ApplicationCompone
         }
         return $result;
     }
+    /* }}} */
+
+    /* public doUninstallAction($args) {{{ */
+    /**
+     * Executes component uninstall action.
+     *
+     * It should be called by uninstall() method only. It should be redefined
+     * by the extending class.
+     *
+     * @param array $params Parameters in the component definition
+     * @access public
+     * @return bool True if not extended.
+     */
     public function doUninstallAction($args)
     {
         $result = false;
+
         $args['file'] = $this->basedir . '/core/classes/shared/maintenance/' . basename($args['file']);
+
         if (strlen($args['name'])) {
-            $result = $this->container->getDataAccess()->execute('DELETE FROM maintenance_tasks WHERE name=' . $this->container->getDataAccess()->formatText($args['name']));
+            $result = $this->container->getDataAccess()->execute(
+                'DELETE FROM maintenance_tasks ' .
+                'WHERE name=' . $this->container->getDataAccess()->formatText($args['name'])
+            );
+
             @unlink($this->container->getHome() . 'core/classes/shared/maintenance/' . $args['file']);
         }
+
         return $result;
     }
+    /* }}} */
+
+    /* public doUpdateAction($args) {{{ */
+     /**
+     * Executes component update action.
+     *
+     * It should be called by update() method only. It should be redefined
+     * by the extending class.
+     *
+     * @param array $params Parameters in the component definition
+     * @access public
+     * @return bool True if not extended.
+     */
     public function doUpdateAction($args)
     {
         $result = false;
+
         $args['file'] = $this->basedir . '/core/classes/shared/maintenance/' . basename($args['file']);
+
         if (strlen($args['name']) and strlen($args['file']) and file_exists($args['file'])) {
             if (! isset($args['catalog'])) {
                 $args['catalog'] = '';
             }
+
             $result = $this->container->getDataAccess()->execute('UPDATE maintenance_tasks SET file=' . $this->container->getDataAccess()->formatText(basename($args['file'])) . ',catalog=' . $this->container->getDataAccess()->formatText($args['catalog']) . ' WHERE name=' . $this->container->getDataAccess()->formatText($args['name']));
+
             if ($result) {
                 @copy($args['file'], $this->container->getHome() . 'core/classes/shared/maintenance/' . basename($args['file']));
                 @chmod($this->container->getHome() . 'core/classes/shared/maintenance/' . basename($args['file']), 0644);
             }
         }
+
         return $result;
     }
+    /* }}} */
 }
--- source/innomatic/core/classes/shared/components/WebappconfigurationComponent.php
+++ source/innomatic/core/classes/shared/components/WebappconfigurationComponent.php
@@ -22,31 +22,89 @@ use \Innomatic\Core;
  */
 class WebappconfigurationComponent extends \Innomatic\Application\ApplicationComponent
 {
+    /* public __construct($rootda, $domainida, $appname, $name, $basedir) {{{ */
+    /**
+     * Class constructor.
+     *
+     * @param \Innomatic\Dataaccess\DataAccess $rootda Innomatic root data access.
+     * @param \Innomatic\Dataaccess\DataAccess $domainda Tenant data access.
+     * @param string $appname Application name identifier.
+     * @param string $name Component name.
+     * @param string $basedir Temporary directory containing the application extracted archive.
+     */
     public function __construct($rootda, $domainida, $appname, $name, $basedir)
     {
         parent::__construct($rootda, $domainida, $appname, $name, $basedir);
     }
+    /* }}} */
 
+    /* public getType() {{{ */
+    /**
+     * Gets the component type identifier string.
+     *
+     * @static
+     * @access public
+     * @return string
+     */
     public static function getType()
     {
         return 'webappconfiguration';
     }
+    /* }}} */
 
+    /* public getPriority() {{{ */
+    /**
+     * getType
+     *
+     * @static
+     * @access public
+     * @return void
+     */
     public static function getPriority()
     {
         return 10;
     }
+    /* }}} */
 
+    /* public getIsDomain() {{{ */
+    /**
+     * Checks if the component can be enabled to/disabled from tenants.
+     *
+     * @static
+     * @access public
+     * @return boolean
+     */
     public static function getIsDomain()
     {
         return true;
     }
+    /* }}} */
 
+    /* public getIsOverridable() {{{ */
+    /**
+     * Checks if the component supports custom overrides.
+     *
+     * @static
+     * @access public
+     * @return boolean
+     */
     public static function getIsOverridable()
     {
         return false;
     }
+    /* }}} */
 
+    /* public doInstallAction($params) {{{ */
+    /**
+     * Executes component install action.
+     *
+     * It should be called by install() method only. It should be redefined
+     * by the extending class.
+     *
+     * @param array $params Parameters in the component definition
+     * @access public
+     * @return bool True if not extended.
+     */
     public function doInstallAction($params)
     {
         $result = false;
@@ -54,12 +112,16 @@ class WebappconfigurationComponent extends \Innomatic\Application\ApplicationCom
             $file = $this->basedir . '/core/conf/' . basename($params['file']);
 
             if (!file_exists(InnomaticContainer::instance('\Innomatic\Core\InnomaticContainer')->getHome() . 'core/applications/' . $this->appname . '/conf/')) {
-                DirectoryUtils::mkTree(InnomaticContainer::instance('\Innomatic\Core\InnomaticContainer')->getHome() . 'core/applications/' . $this->appname . '/conf/', 0755);
+                DirectoryUtils::mkTree(
+                    InnomaticContainer::instance('\Innomatic\Core\InnomaticContainer')->getHome() .
+                    'core/applications/' . $this->appname . '/conf/', 0755
+                );
             }
 
             if (copy(
                 $file,
-                InnomaticContainer::instance('\Innomatic\Core\InnomaticContainer')->getHome() . 'core/applications/' . $this->appname . '/conf/' .basename($file)
+                InnomaticContainer::instance('\Innomatic\Core\InnomaticContainer')->getHome() .
+                'core/applications/' . $this->appname . '/conf/' .basename($file)
             )) {
                 $result = true;
             }
@@ -72,7 +134,19 @@ class WebappconfigurationComponent extends \Innomatic\Application\ApplicationCom
         }
         return $result;
     }
+    /* }}} */
 
+    /* public doUninstallAction($params) {{{ */
+    /**
+     * Executes component uninstall action.
+     *
+     * It should be called by uninstall() method only. It should be redefined
+     * by the extending class.
+     *
+     * @param array $params Parameters in the component definition
+     * @access public
+     * @return bool True if not extended.
+     */
     public function doUninstallAction($params)
     {
         $result = false;
@@ -95,7 +169,19 @@ class WebappconfigurationComponent extends \Innomatic\Application\ApplicationCom
         }
         return $result;
     }
+    /* }}} */
 
+    /* public doUpdateAction($params) {{{ */
+     /**
+     * Executes component update action.
+     *
+     * It should be called by update() method only. It should be redefined
+     * by the extending class.
+     *
+     * @param array $params Parameters in the component definition
+     * @access public
+     * @return bool True if not extended.
+     */
     public function doUpdateAction($params)
     {
         $result = false;
@@ -130,7 +216,20 @@ class WebappconfigurationComponent extends \Innomatic\Application\ApplicationCom
         }
         return $result;
     }
+    /* }}} */
 
+    /* public doEnableDomainAction($domainid, $params) {{{ */
+    /**
+     * Executes enable domain action.
+     *
+     * It should be called by enable() method only. It should be redefined
+     * by the extending class.
+     *
+     * @param string $domainid Domain identifier name
+     * @param array $params Parameters in the component definition
+     * @access public
+     * @return void
+     */
     public function doEnableDomainAction($domainid, $params)
     {
         $domainQuery = $this->rootda->execute("SELECT domainid FROM domains WHERE id={$domainid}");
@@ -140,7 +239,8 @@ class WebappconfigurationComponent extends \Innomatic\Application\ApplicationCom
 
         $domain = $domainQuery->getFields('domainid');
 
-        $fileDestName = RootContainer::instance('\Innomatic\Core\RootContainer')->getHome().$domain.'/core/conf/'.basename($params['file']);
+        $fileDestName = RootContainer::instance('\Innomatic\Core\RootContainer')->getHome() .
+            $domain . '/core/conf/' . basename($params['file']);
 
         if (!file_exists(RootContainer::instance('\Innomatic\Core\RootContainer')->getHome().$domain.'/core/conf/')) {
             DirectoryUtils::mkTree(RootContainer::instance('\Innomatic\Core\RootContainer')->getHome().$domain.'/core/conf/', 0755);
@@ -154,23 +254,40 @@ class WebappconfigurationComponent extends \Innomatic\Application\ApplicationCom
         }
         return true;
     }
+    /* }}} */
 
+    /* public doUpdateDomainAction($domainid, $params) {{{ */
+    /**
+     * Executes update domain action.
+     *
+     * It should be called by update() method only. It should be redefined
+     * by the extending class.
+     *
+     * @param string $domainid Domain identifier name
+     * @param array $params Parameters in the component definition
+     * @access public
+     * @return void
+     */
     public function doUpdateDomainAction($domainid, $params)
     {
-        $domainQuery = $this->rootda->execute("SELECT domainid FROM domains WHERE id={$domainid}");
+        $domainQuery = $this->rootda->execute(
+            "SELECT domainid FROM domains WHERE id={$domainid}"
+        );
+
         if (!$domainQuery->getNumberRows()) {
             return false;
         }
 
         $domain = $domainQuery->getFields('domainid');
 
-        $fileDestName = RootContainer::instance('\Innomatic\Core\RootContainer')->getHome().$domain.'/core/conf/'.basename($params['file']);
+        $fileDestName = RootContainer::instance('\Innomatic\Core\RootContainer')->getHome() .
+            $domain . '/core/conf/' . basename($params['file']);
 
         // Checks if the "keep" parameter is set to true.
         // If so, the configuration file will not be overwritten.
         if (isset($params['keep']) and $params['keep'] == true and file_exists($fileDestName)) {
             return true;
-        } 
+        }
 
         if (!file_exists(RootContainer::instance('\Innomatic\Core\RootContainer')->getHome().$domain.'/core/conf/')) {
             DirectoryUtils::mkTree(RootContainer::instance('\Innomatic\Core\RootContainer')->getHome().$domain.'/core/conf/', 0755);
@@ -185,17 +302,34 @@ class WebappconfigurationComponent extends \Innomatic\Application\ApplicationCom
         return true;
 
     }
+    /* }}} */
 
+    /* public doDisableDomainAction($domainid, $params) {{{ */
+    /**
+     * Executes disable domain action.
+     *
+     * It should be called by disable() method only. It should be redefined
+     * by the extending class.
+     *
+     * @param string $domainid Domain identifier name
+     * @param array $params Parameters in the component definition
+     * @access public
+     * @return void
+     */
     public function doDisableDomainAction($domainid, $params)
     {
-        $domainQuery = $this->rootda->execute("SELECT domainid FROM domains WHERE id={$domainid}");
+        $domainQuery = $this->rootda->execute(
+            "SELECT domainid FROM domains WHERE id={$domainid}"
+        );
+
         if (!$domainQuery->getNumberRows()) {
             return false;
         }
 
         $domain = $domainQuery->getFields('domainid');
 
-        $fileDestName = RootContainer::instance('\Innomatic\Core\RootContainer')->getHome().$domain.'/core/conf/'.basename($params['file']);
+        $fileDestName = RootContainer::instance('\Innomatic\Core\RootContainer')->getHome() .
+            $domain . '/core/conf/' . basename($params['file']);
 
         if (file_exists($fileDestName)) {
             return unlink($fileDestName);
@@ -203,5 +337,5 @@ class WebappconfigurationComponent extends \Innomatic\Application\ApplicationCom
             return false;
         }
     }
-
+    /* }}} */
 }
