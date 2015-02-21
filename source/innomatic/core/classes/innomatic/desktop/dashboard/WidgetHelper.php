e584e97636f762e31e6cda337b39cb373036c0a8
Alex Pagnoni <alex.pagnoni@innoteam.it>
2015-02-21 20:22:25 +0100

    Added code comments

--- source/innomatic/core/classes/innomatic/desktop/dashboard/WidgetHelper.php
+++ source/innomatic/core/classes/innomatic/desktop/dashboard/WidgetHelper.php
@@ -17,7 +17,7 @@ use \Innomatic\Desktop\Auth\DesktopPanelAuthorizator;
 
 /**
  * Helper methods for desktop dashboard widgets.
- * 
+ *
  * @since 7.0.0
  * @author Alex Pagnoni <alex.pagnoni@innomatic.io>
  */
@@ -25,13 +25,14 @@ class WidgetHelper
 {
     /**
      * Builds the WUI xml string for the given dashboard widget.
-     * 
+     *
      * @since 7.0.0
      * @param string $name Widget name.
      * @return string
      */
     public static function getWidgetXml($name)
     {
+        // Default WUI XML definition
         $xml       = '<void/>';
         $container = \Innomatic\Core\InnomaticContainer::instance('\Innomatic\Core\InnomaticContainer');
         $domain_da = $container->getCurrentDomain()->getDataAccess();
@@ -50,6 +51,7 @@ WHERE name='.$domain_da->formatText($name)
 
             // Do not show widgets tied to a panel when the panel is not accessible to the current user
             if (strlen($panel)) {
+                // Check permission
                 $node_id = $perm->getNodeIdFromFileName($panel);
                 if (
                     $perm->check($node_id, DesktopPanelAuthorizator::NODETYPE_PAGE )
@@ -59,10 +61,13 @@ WHERE name='.$domain_da->formatText($name)
                 }
             }
 
+            // Check if the current widget is allowed
             if ($allowed) {
+                // Get class field
                 $class = $widget_query->getFields('class');
 
                 // Check if the class exists
+                // @todo must be updated to new class definition with namespace
                 if (class_exists($class, true)) {
                     // Fetch the widget xml definition
                     $widget = new $class;
@@ -73,11 +78,11 @@ WHERE name='.$domain_da->formatText($name)
 
         return $xml;
     }
-    
+
     /**
      * Returns the list of the available dashboard widgets for the current
      * tenant user.
-     * 
+     *
      * @return array Array of the available widgets.
      */
     public function getWidgetsList()
@@ -86,7 +91,7 @@ WHERE name='.$domain_da->formatText($name)
         $domain_da = $container->getCurrentDomain()->getDataAccess();
         $perm      = new DesktopPanelAuthorizator($domain_da, $container->getCurrentUser()->getGroup());
         $widgets   = array();
-        
+
         // Extract the list of all the widgets
         $widget_query = $domain_da->execute('SELECT * FROM domain_dashboards_widgets');
 
